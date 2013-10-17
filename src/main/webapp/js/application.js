9b925f69f57b95b2f8bcac9fa3b72ee9442bb37e
mathieu-pousse <mathieu.pousse@zenika.com>
2013-10-17 19:00:04 +0200

    try to make it a bit more userfriendly..., try...

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -4,7 +4,8 @@
 var application = angular.module("iwb", ["ngRoute"]);
 
 application.config(["$routeProvider", function($routeProvider) {
-    $routeProvider.when("/home", {templateUrl: "fragments/home.html", controller: SearchController});
+    $routeProvider.when("/home", {templateUrl: "fragments/home.html"});
+    $routeProvider.when("/search", {templateUrl: "fragments/search.html", controller: SearchController});
     $routeProvider.when("/products/search", {templateUrl: "fragments/products.html", controller: ProductsController});
     $routeProvider.when("/products/id/:id", {templateUrl: "fragments/product.html", controller: ProductController});
     $routeProvider.otherwise({redirectTo : "/home"});
